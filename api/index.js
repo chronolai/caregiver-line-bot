d40c4f32149becf77dec2694f26e01ee78f8799b
chrono.lai <chrono.lai@cycarrier.com>
2023-03-02 18:46:53 +0800

    Update routes for API and webhook and add UUID to api response

--- api/index.js
+++ api/index.js
@@ -1,9 +1,24 @@
 const app = require('express')();
+const { v4 } = require('uuid');
 
-app.get('/webhook', (req, res) => {
-  res.end('Hello');
+app.get('/api', (req, res) => {
+  const path = `/api/item/${v4()}`;
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
 });
 
-app.listen(3000, () => {
-    console.log(`Example app listening on port ${3000}`)
-})
+app.get('/api/item/:slug', (req, res) => {
+  const { slug } = req.params;
+  res.end(`Item: ${slug}`);
+});
+
+app.get('/webhook', (req, res) => {
+  res.end(`Hello webhook`);
+});
+
+app.get('/', (req, res) => {
+  res.end(`OK`);
+});
+
+module.exports = app;
