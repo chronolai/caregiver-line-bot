2b462adb7cfc42c90d1d34829532904d3e2778f1
chrono.lai <chrono.lai@cycarrier.com>
2023-03-18 13:58:35 +0800

    Update index.js to use v2.Translate, add ameia function for translation and add reply with translated message in webhook post route.

--- api/index.js
+++ api/index.js
@@ -3,9 +3,7 @@ import { v2 } from '@google-cloud/translate';
 
 import { reply } from '../services/line.js';
 
-const { Translate } = v2;
-
-const lib = new Translate({
+const lib = new v2.Translate({
   projectId: process.env.PROJECT_ID,
   key: process.env.API_KEY,
 });
@@ -13,16 +11,36 @@ const lib = new Translate({
 async function detect(text) {
   const res = await lib.detect(text);
   const [detection] = res;
+  const { language } = detection;
+  if (language === 'zh-CN') return 'zh-TW';
   return detection.language;
 }
 
-const app = express();
+async function translate(text, target = 'zh-TW') {
+  const [translation] = await lib.translate(text, target);
+  return translation;
+}
 
+async function ameia(text) {
+  console.error('ameia', text);
+  const mapping = { 'id': 'zh-TW', 'zh-TW': 'id', 'zh-CN': 'id', 'en': 'id' };
+  const source = await detect(text);
+  const target = source === 'en' ? 'en' : mapping[source];
+  const result = source === 'en' ? text : await translate(text, target);
+  return {
+    source_locale: source,
+    source_text: text,
+    target_locale: target,
+    target_text: result,
+  };
+}
+
+const app = express();
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  detect('你好').then((txt) => {
-    res.send(JSON.stringify(txt));
+  ameia('你好').then((result) => {
+    res.json(result);
   });
 });
 
@@ -30,9 +48,10 @@ app.post('/webhook', async (req, res) => {
   const events = req.body.events || [];
   const replies = events
     .filter(({ type }) => type === 'message')
-    .map(({ replyToken, message }) => reply({
-      replyToken,
-      messages: [{ type: 'text', text: message.text }],
+    .map(({ replyToken, message }) => ameia(message.text).then((result) => {
+      console.error(result);
+      const data = { replyToken, messages: [{ type: 'text', text: result.target_text }] };
+      return reply(data);
     }));
   await Promise.all(replies);
   res.sendStatus(200);
