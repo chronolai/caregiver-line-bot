38a972dd215cdb9595d5f9f88be1de3661fc0b2c
chrono.lai <chrono.lai@cycarrier.com>
2023-03-18 13:20:11 +0800

    Add Google Translate API integration, including package dependency, detect function and API endpoint

--- api/index.js
+++ api/index.js
@@ -1,12 +1,29 @@
 import express from 'express';
+import { v2 } from '@google-cloud/translate';
+
 import { reply } from '../services/line.js';
+
+const { Translate } = v2;
+
+const lib = new Translate({
+  projectId: process.env.PROJECT_ID,
+  key: process.env.API_KEY,
+});
+
+async function detect(text) {
+  const res = await lib.detect(text);
+  const [detection] = res;
+  return detection.language;
+}
 
 const app = express();
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.sendStatus(200);
+  detect('你好').then((txt) => {
+    res.send(JSON.stringify(txt));
+  });
 });
 
 app.post('/webhook', async (req, res) => {
